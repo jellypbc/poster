fed253756e939db8716a52a834947cad5131366e
cindy <ciindy.wu@gmail.com>
2019-11-25 14:50:29 -1000

    Adds authors as a column to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_003336) do
+ActiveRecord::Schema.define(version: 2019_11_26_004956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_003336) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
     t.string "publisher"
+    t.string "authors"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
