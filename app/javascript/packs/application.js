590ce15f9774e74fd0dc626a7bc6b43ce37e2f97
Denny Luan <dennyluan@gmail.com>
2019-11-25 23:13:50 -1000

    Remove server rendering webpacker files for production

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,19 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// require("@rails/ujs").start()
+require("@rails/ujs").start()
 require("turbolinks").start()
-// require("@rails/activestorage").start()
-// require("channels")
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-// Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
