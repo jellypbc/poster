34fe592afd193f0db78143919f74e35bee907967
Denny Luan <dennyluan@gmail.com>
2019-12-06 21:52:16 -1000

    Set redis provider env

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 $redis =
-  if ENV['REDIS_PROVIDER']
-    Redis.new(url: ENV['REDIS_PROVIDER'])
+  if ENV['REDISTOGO_URL']
+    Redis.new(url: ENV['REDISTOGO_URL'])
   else
     Redis::Namespace.new(Rails.env, redis: Redis.new)
   end
