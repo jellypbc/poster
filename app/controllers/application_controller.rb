85ed3c835de6ea1b0ef90f6474f3255559967bc1
Alex Krolick <alexander.krolick@gmail.com>
2019-12-19 21:11:50 -0800

    disable auth requirement for demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
-	# NOTE: Some pages may be exempt from immediate authentication,
+	# TODO: reenable authenticaion
+	# disabled for hosted demo
+	# Some pages may be exempt from immediate authentication,
 	# ie, if there is a help or info page served by Rails,
 	# or if you can create an anonymous post. If this happens,
-	# move this filter or create a new base controller without it.
-	before_action :authenticate_user!
+	# move this filter or create a new base controller without it
+	# before_action :authenticate_user!
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	layout :layout_by_resource
