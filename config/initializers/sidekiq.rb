0997abd9f9d4c34d04304b1beffe7de9f4269749
Denny Luan <dennyluan@gmail.com>
2020-01-04 00:35:36 -1000

    Fix sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,18 +5,18 @@
   #   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
   # end
 # end
-
+uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
 
 Sidekiq.configure_server do |config|
   config.redis = {
-  	url: 'redis://localhost:6379/',
+  	url: uri,
   	network_timeout: 5
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-  	url: 'redis://localhost:6379/',
+  	url: uri,
   	network_timeout: 5
   }
 end
