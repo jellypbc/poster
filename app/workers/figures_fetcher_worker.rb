6ab0f87c33009b637bdce5c58bc0876bd704a285
Denny Luan <dennyluan@gmail.com>
2020-01-04 00:23:58 -1000

    More misc cleanup

--- app/workers/figures_fetcher_worker.rb
+++ app/workers/figures_fetcher_worker.rb
@@ -5,23 +5,22 @@ class FiguresFetcherWorker
 
   # TODO:
   # error handling for when image.save! fails
-
-	# Fetches figures from external figure server and saves them
+  # Fetches figures from external figure server and saves them
   def perform(body, upload_id)
-  	@upload = Upload.find upload_id
+    @upload = Upload.find upload_id
     @upload.upload_images.destroy_all if @upload.upload_images.any?
 
     body.each do |figure|
-    	upload_image = @upload.upload_images.new
+      upload_image = @upload.upload_images.new
 
       file_path = figure["renderURL"]
-    	url = FIGURES_HOST + '/' + file_path
+      url = FIGURES_HOST + '/' + file_path
 
       upload_image.type = figure["figType"]
       upload_image.caption = figure["caption"]
       upload_image.page = figure["page"]
       upload_image.name = figure["name"]
-    	upload_image.image_remote_url = url
+      upload_image.image_remote_url = url
 
       upload_image.save!
     end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,2 @@
 uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
 $redis = Redis.new(url: uri)
-# $redis = Redis.new(Rails.application.config(:redis))
