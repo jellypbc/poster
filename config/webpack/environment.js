119dabbd1148189338923e03abaf94700146a64d
Denny Luan <dennyluan@gmail.com>
2020-01-28 00:49:46 -1000

    Fix webpack config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,5 @@ const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 
 environment.loaders.prepend('typescript', typescript)
+environment.loaders.delete('nodeModules')
 module.exports = environment
