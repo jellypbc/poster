97768a4ed97185bf6fd2586a5f8cd2c95ab5a6c6
Denny Luan <dennyluan@gmail.com>
2020-02-13 13:40:46 -1000

    Auth for some user actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :fetch_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:index, :edit, :destroy, :update]
 
   def index
     @users = User.all
