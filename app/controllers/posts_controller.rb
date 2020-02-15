4c2bbaf847e7ac398a04f2913d45f835ca17437a
cindy <ciindy.wu@gmail.com>
2020-02-14 21:36:07 -0800

    only show posts that are pdf uploads
    i'm getting lost in the references posts when demoing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.primary
       .order(created_at: :desc)
       .paginate(page: params[:page], per_page: 40)
   end
