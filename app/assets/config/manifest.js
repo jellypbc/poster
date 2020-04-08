bad8636eeaf9632abb18298d8cf78359ab5c70f6
Denny Luan <dennyluan@gmail.com>
2020-04-08 00:17:03 -1000

    Updates assets manifest file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,4 @@
+// See issue: https://github.com/rails/sprockets/issues/474#issuecomment-374958102
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link application.js
+//= link application.css
