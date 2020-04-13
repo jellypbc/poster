78499b021b052d3eb9f64ce64594de9aa7f9301b
Alex Krolick <alexander.krolick@gmail.com>
2020-04-12 19:03:40 -0700

    refactor pm + app state effect pattern

--- app/javascript/components/editor-config/images.js
+++ app/javascript/components/editor-config/images.js
@@ -1,21 +1,35 @@
-import { store } from '../store'
+import { store, waitForStore, TIMEOUT_ERROR } from '../store'
 import schema from './schema'
 
-export const addFigure = function (state, dispatch) {
-  console.log(state)
-  if (dispatch) {
-    store.dispatch({
-      type: 'addImageStart',
-      payload: {
-        onImageAddSuccess: (action) => {
-          var imageUrl = action.payload.fileUrl
-          var imageType = schema.nodes.image
-          let tr = state.tr
-          dispatch(tr.replaceSelectionWith(imageType.create({ src: imageUrl })))
-          return true
-        },
-      },
+// note: "pm" = ProseMirror, "store" = app-wide redux store
+export const addFigure = function (pmState, pmDispatch) {
+  if (!pmDispatch) return true
+
+  store.dispatch({
+    type: 'addImageStart',
+  })
+
+  waitForStore({
+    selector: (storeState) => storeState.images,
+    succeed: (images) => !images.isAddingImage && images.lastImage,
+    fail: (images) => !images.isAddingImage && !images.lastImage,
+    timeout: 1000 * 30,
+  })
+    .then((imageStoreState) => {
+      const imageUrl = imageStoreState.lastImage.fileUrl
+      const imageType = schema.nodes.image
+      pmDispatch(
+        pmState.tr.replaceSelectionWith(imageType.create({ src: imageUrl }))
+      )
     })
-  }
+    .catch((err) => {
+      if (err.message === TIMEOUT_ERROR) {
+        store.dispatch({
+          type: 'closeImageModal',
+        })
+      }
+      // else, they closed the modal manually (or got an unrecoverable error)
+    })
+
   return true
 }
--- app/javascript/components/store.js
+++ app/javascript/components/store.js
@@ -36,26 +36,18 @@ const commentReducers = {
   },
 }
 
-const imagesEffects = {
-  onImageAddSuccess: (payload) => {},
-}
-
 const imagesReducerDefaultState = {
   isAddingimage: false,
+  lastImage: null,
 }
 
 const imageReducers = {
   addImageStart: (state, action) => {
+    state.lastImage = null
     state.isAddingImage = true
-    // runs the images plugin onImageAddSuccess() payload
-    if (action.payload.onImageAddSuccess) {
-      imagesEffects.onImageAddSuccess = action.payload.onImageAddSuccess
-    }
   },
   addImageSuccess: (state, action) => {
-    if (imagesEffects.onImageAddSuccess) {
-      imagesEffects.onImageAddSuccess(action)
-    }
+    state.lastImage = action.payload
     state.isAddingImage = false
   },
   closeImageModal: (state, action) => {
@@ -73,4 +65,70 @@ const store = configureStore({
 // for debugging
 window._store = store
 
-export { store }
+/**
+ * Function to create a store observer
+ * @param {(State | SelectorResult) => any} onChange - function to call on state change
+ * @param {{}} [options] - options object
+ * @param {(State) => SelectorResult} [options.selector] - optional function to transform the store state
+ * @returns {{ unsubscribe: () => void }} object with an unsubscribe method
+ */
+function observeStore(onChange, { selector = (state) => state } = {}) {
+  let currentState
+
+  function handleChange() {
+    // Using a selector function lets us subscribe to changes in just a slice of
+    // the store (defaults to the whole thing if no selector is provided):
+    let nextState = selector(store.getState())
+    if (nextState !== currentState) {
+      currentState = nextState
+      onChange(currentState)
+    }
+  }
+
+  let unsubscribe = store.subscribe(handleChange)
+  handleChange()
+  return { unsubscribe }
+}
+
+// exported in case you need to check the reason the condition failed
+const TIMEOUT_ERROR = 'condition_timed_out'
+
+/**
+ * @example await waitForStore({ selector: state => state.posts[0], timeout: 1000, succeed: newPost => newPost.isCreating === false})
+ * @returns {Promise} - Promise that resolves when succeed option returns true, and rejects if it times out or fail option returns true
+ * @param {{}} [options] - options object
+ * @param {(SelectorResult) => boolean} [options.succeed] - take selector result and returns true if promise should resolve
+ * @param {(SelectorResult) => boolean} [options.fail] - take selector result and returns true if promise should reject
+ * @param {(State) => SelectorResult} [options.selector] - optional function to transform the store state
+ * @param {number} [options.timeout] how long to wait before rejecting automatically
+ */
+function waitForStore({
+  succeed = () => false,
+  fail = () => false,
+  timeout = 5000,
+  selector = (state) => state,
+} = {}) {
+  return new Promise((resolvePromise, rejectPromise) => {
+    let observer
+    const timer = setTimeout(() => {
+      observer.unsubscribe()
+      rejectPromise(new Error(TIMEOUT_ERROR))
+    }, timeout)
+    const finish = () => {
+      observer.unsubscribe()
+      clearTimeout(timer)
+    }
+    const onChange = (state) => {
+      if (succeed(state)) {
+        finish()
+        resolvePromise(state)
+      } else if (fail(state)) {
+        finish()
+        rejectPromise(state)
+      }
+    }
+    observer = observeStore(onChange, selector)
+  })
+}
+
+export { store, observeStore, waitForStore, TIMEOUT_ERROR }
