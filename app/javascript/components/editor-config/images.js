90e5786657fc8c60cf2e42eee82a010ded4470b6
Alex Krolick <alexander.krolick@gmail.com>
2020-04-14 01:38:28 -0700

    rename vars more simply

--- app/javascript/components/editor-config/images.js
+++ app/javascript/components/editor-config/images.js
@@ -1,17 +1,15 @@
 import schema from './schema'
 
 // note: "pm" = ProseMirror, "store" = app-wide redux store
-export const addFigure = function (pmState, pmDispatch) {
-  if (!pmDispatch) return true
+export const addFigure = function (state, dispatch) {
+  if (!dispatch) return true
 
   window.dispatchEvent(new CustomEvent('ImageUploadRequested'))
 
   const handleUpload = (evt) => {
     const imageUrl = evt.details.fileUrl
     const imageType = schema.nodes.image
-    pmDispatch(
-      pmState.tr.replaceSelectionWith(imageType.create({ src: imageUrl }))
-    )
+    dispatch(state.tr.replaceSelectionWith(imageType.create({ src: imageUrl })))
     window.removeEventListener('ImageUploadCompleted', handleUpload)
   }
   window.addEventListener('ImageUploadCompleted', handleUpload)
