95c13610067c4965b638f20906df95b4e50856fe
Denny Luan <dennyluan@gmail.com>
2020-07-09 13:46:48 -1000

    Show backlinks for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -39,6 +39,7 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true
 	has_many :uploads
 	has_many :citations
+  has_many :backlinks, class_name: 'Citation', foreign_key: 'generated_post_id'
   has_many :figures, through: :uploads, source: :upload_figures, class_name: 'UploadFigure'
 
   has_many :tag_ownerships
