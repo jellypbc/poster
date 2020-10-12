313bcfd49314c486a308c94078f7c8ab9a1b5246
cindy <ciindy.wu@gmail.com>
2020-10-11 19:20:20 -1000

    Adds commented out WIP TagForm function component

--- app/javascript/components/TagForm.js
+++ app/javascript/components/TagForm.js
@@ -1,6 +1,107 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { WithContext as ReactTags } from 'react-tag-input'
 import saRequest from '../utils/saRequest'
+
+// TODO: figure out how to handle async function and promise :(
+
+// export default function TagForm({ post, suggestedTags, currentUser }) {
+//   const [tags, setTags] = useState(post.data.attributes.tags || [])
+//   const [suggestions, setSuggestions] = useState(suggestedTags || [])
+//   const [error, setError] = useState(null)
+
+//   const handleFocus = () => {
+//     let url = post.data.attributes.form_url + '/suggested_tags'
+//     saRequest
+//       .get(url)
+//       .set('accept', 'application/json')
+//       .then((res) => {
+//         if (res.status === 200) {
+//           setSuggestions(res.body)
+//         } else {
+//           setError('Oops, failed to fetch suggested tags')
+//         }
+//       })
+//   }
+//   const handleDelete = (i) => {
+//     const tagToDelete = tags.find((tag, index) => index === i)
+//     sendRequest(tagToDelete, 'delete').then((res) => {
+//       if (res.status === 200) {
+//         setTags(tags.filter((tag, index) => index !== i))
+//       } else {
+//         setError('Oops, something went wrong.')
+//       }
+//     })
+//   }
+
+//   // do we need to do the prevTags thing
+//   const handleAddition = (tag) => {
+//     sendRequest(tag, 'add').then((res) => {
+//       if (res.status === 200) {
+//         setTags([...tags, tag])
+//       } else {
+//         setError('Oops, something went wrong.')
+//       }
+//     })
+//   }
+
+//   useEffect(() => {
+//     async function sendRequest(tag, action) {
+//       const data = {
+//         tag: {
+//           user_id: currentUser.id,
+//           text: tag.text,
+//           slug: tag.slug,
+//           post_id: post.data.id,
+//         },
+//       }
+//       let url
+//       let method
+//       if (action === 'add') {
+//         url = post.data.attributes.form_url + '/tags'
+//         method = 'post'
+//       } else {
+//         data.tag.id = tag.id
+//         url = post.data.attributes.form_url + '/tags/' + tag.id
+//         method = 'delete'
+//       }
+
+//       return new Promise(function (resolve, reject) {
+//         saRequest[method](url)
+//           .send(data)
+//           .set('accept', 'application/json')
+//           .then((res) => {
+//             return res
+//           })
+//           .then((result) => {
+//             resolve(result)
+//           })
+//           .catch((err) => {
+//             setError(err.message)
+//           })
+//       })
+//     }
+//   })
+
+//   return (
+//     <div className="form-group">
+//       {error && <div className="error">{error}</div>}
+//       <ReactTags
+//         tags={tags}
+//         suggestions={suggestions}
+//         handleDelete={handleDelete}
+//         handleAddition={handleAddition}
+//         handleInputFocus={() => handleFocus()}
+//         allowDragDrop={false}
+//         placeholder="Add new tag"
+//         autofocus={false}
+//         classNames={{
+//           tag: 'badge badge-secondary mr-2',
+//           tagInputField: 'form-control',
+//         }}
+//       />
+//     </div>
+//   )
+// }
 
 class TagForm extends React.Component {
   constructor(props) {
@@ -8,7 +109,7 @@ class TagForm extends React.Component {
 
     this.state = {
       tags: this.props.post.data.attributes.tags || [],
-      suggestions: this.props.suggested_tags || [],
+      suggestions: this.props.suggestedTags || [],
     }
 
     this.handleDelete = this.handleDelete.bind(this)
