45ec26339dd75e7ae1d7db2531fe90ecb447de59
cindy <ciindy.wu@gmail.com>
2020-10-11 20:08:29 -1000

    Refactor FollowButton to function component wip

--- app/javascript/components/followbutton.js
+++ app/javascript/components/followbutton.js
@@ -1,15 +1,125 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import saRequest from '../utils/saRequest'
 import Modal from 'react-modal'
 
 // TODO: add debounce and rate limiter
+//{ objectId, objectType, following, buttonClass, currentUser }
+// export default function FollowButton(props) {
+//   const { objectId, objectType, currentUser } = props
+
+//   const [following, setFollowing] = useState(props.following || false)
+//   const [showModal, setShowModal] = useState(false)
+//   const [error, setError] = useState(null)
+
+//   Modal.setAppElement('#root')
+
+//   useEffect(() => {
+//     let el = document.querySelector('.hide-follow-button')
+//     el.parentNode.removeChild(el)
+//   })
+
+//   const followData = () => {
+//     return {
+//       user_id: currentUser,
+//       follow: {
+//         following_type: objectType,
+//         following_id: objectId,
+//       },
+//     }
+//   }
+
+//   const setFollow = () => {
+//     if (currentUser) {
+//       let data = followData()
+//       let url = '/users/' + objectId + '/follow'
+
+//       saRequest
+//         .post(url)
+//         .send(data)
+//         .set('accept', 'application/json')
+//         .then((res) => {
+//           setFollowing(true)
+//         })
+//         .catch((err) => {
+//           console.log('err.message', err.message)
+//           setError(err.message)
+//         })
+//     }
+//   }
+
+//   const setUnfollow = () => {
+//     if (currentUser) {
+//       let data = followData()
+//       let url = '/users/' + objectId + '/unfollow'
+
+//       saRequest
+//         .post(url)
+//         .send(data)
+//         .set('accept', 'application/json')
+//         .then((res) => {
+//           setFollowing(false)
+//         })
+//         .catch((err) => {
+//           console.log('err.message', err.message)
+//           setError(err.message)
+//         })
+//     }
+//   }
+
+//   const handleClick = (e) => {
+//     if (!currentUser) {
+//       setShowModal(true)
+//     } else {
+//       e.preventDefault()
+//       e.stopPropagation()
+//       return following ? setUnfollow() : setFollow()
+//     }
+//   }
+
+//   let classNames = 'btn btn-primary'
+//   if (following) {
+//     classNames += ' following'
+//   }
+
+//   return (
+//     <div>
+//       <Modal
+//         isOpen={showModal}
+//         className="clear-modal-container"
+//         // onRequestClose={() => this.setState({ showModal: false })}
+//         onRequestClose={() => setShowModal(false)}
+//         shouldCloseOnOverlayClick={true}
+//       >
+//         <div className="reg-modal-container animated fadeIn">
+//           <div className="login-message ">
+//             <p>You must be logged in to follow this user.</p>
+//           </div>
+//         </div>
+//       </Modal>
+
+//       <button
+//         className={classNames}
+//         onClick={(e) => handleClick(e)}
+//         id="followbutton"
+//       >
+//         {following && <span></span>}
+//         {!following && (
+//           <span>
+//             <i className="fa fa-user-friends mr-2" />
+//           </span>
+//         )}
+//       </button>
+//     </div>
+//   )
+// }
+
 class FollowButton extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      object_id: this.props.object_id,
-      object_type: this.props.object_type,
+      object_id: this.props.objectId,
+      object_type: this.props.objectType,
       following: this.props.following || false,
       showModal: false,
     }
@@ -18,23 +128,24 @@ class FollowButton extends React.Component {
 
   componentDidMount() {
     var el = document.querySelector('.hide-follow-button')
+    console.log('el', el)
     el.parentNode.removeChild(el)
   }
 
   followData() {
     return {
-      user_id: this.props.current_user,
+      user_id: this.props.currentUser,
       follow: {
-        following_type: this.state.object_type,
-        following_id: this.state.object_id,
+        following_type: this.state.objectType,
+        following_id: this.state.objectId,
       },
     }
   }
 
   setFollow() {
-    if (this.props.current_user) {
+    if (this.props.currentUser) {
       var data = this.followData()
-      var url = '/users/' + this.props.object_id + '/follow'
+      var url = '/users/' + this.props.objectId + '/follow'
 
       saRequest
         .post(url)
@@ -51,9 +162,9 @@ class FollowButton extends React.Component {
   }
 
   setUnfollow() {
-    if (this.props.current_user) {
+    if (this.props.currentUser) {
       var data = this.followData()
-      var url = '/users/' + this.props.object_id + '/unfollow'
+      var url = '/users/' + this.props.objectId + '/unfollow'
 
       saRequest
         .post(url)
@@ -70,7 +181,7 @@ class FollowButton extends React.Component {
   }
 
   handleClick(e) {
-    if (!this.props.current_user) {
+    if (!this.props.currentUser) {
       this.setState({ showModal: true })
     } else {
       e.preventDefault()
