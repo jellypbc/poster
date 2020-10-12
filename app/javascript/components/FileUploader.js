899194073df55e5a466e8e7bf270a7b8f17b68bc
cindy <ciindy.wu@gmail.com>
2020-10-11 20:43:54 -1000

    Refactor FileUploader to WIP function component

--- app/javascript/components/FileUploader.js
+++ app/javascript/components/FileUploader.js
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Uppy from '@uppy/core'
 import XHRUpload from '@uppy/xhr-upload'
 import { DragDrop } from '@uppy/react'
 
 import saRequest from '../utils/saRequest'
+
+// export default function FileUploader({ url }) {
+//   const [fileUrl, setFileUrl] = useState('')
+//   const [id, setId] = useState('')
+//   const [error, setError] = useState(null)
+//   const token = document.head.querySelector('[name~=csrf-token][content]').content
+
+//   const uppy = Uppy({
+//     meta: { type: 'upload' },
+//     restrictions: { maxNumberOfFiles: 1 },
+//     autoProceed: true,
+//   }).use(XHRUpload, {
+//     endpoint: '/file/store',
+//     bundle: true,
+//     headers: {
+//       csrf: token,
+//     },
+//   })
+
+//   uppy.on('complete', (result) => {
+//     const fileUrl = result.successful[0].response.body.url
+//     const id = result.successful[0].response.body.data.id
+//     const mimeType = result.successful[0].response.body.data.metadata.mime_type
+
+//     setFileUrl(fileUrl)
+//     setId(id)
+
+//     if (mimeType === 'application/pdf') {
+//       fireAway()
+//     } else {
+//       setError('Oops, we only accept PDFs for now.')
+//     }
+//   })
+
+//   const fireAway = () => {
+//     const data = {
+//       file_id: id,
+//       upload: {},
+//     }
+//     saRequest
+//       .post(url)
+//       .send(data)
+//       .set('accept', 'application/json')
+//       .then((res) => {
+//         console.log(res)
+//         window.location = res.body.redirect_to
+//       })
+//       .catch((err) => {
+//         console.log(err.message)
+//         setError(err.message)
+//       })
+//   }
+
+//   useEffect(() => {
+//     return () => {
+//       uppy.close()
+//     }
+//   })
+
+//   return (
+//     <div>
+//       {error && <p className="error">{error}</p>}
+
+//       <DragDrop
+//         uppy={uppy}
+//         locale={{
+//           strings: {
+//             // Text to show on the droppable area.
+//             // `%{browse}` is replaced with a link that opens the
+//             // system file selection dialog.
+//             dropHereOr: 'Drop here or %{browse}',
+//             // Used as the label for the link that opens the
+//             // system file selection dialog.
+//             browse: 'choose a file',
+//           },
+//         }}
+//       />
+//     </div>
+//   )
+// }
 
 class FileUploader extends React.Component {
   constructor(props) {
