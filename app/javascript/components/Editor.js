89eabbdeaf3dc190860da0c27f7827002faa8f12
cindy <ciindy.wu@gmail.com>
2020-10-12 19:39:59 -1000

    Fixed Floater component

--- app/javascript/components/Editor.js
+++ app/javascript/components/Editor.js
@@ -4,85 +4,179 @@ import { EditorView } from 'prosemirror-view'
 import { commentPluginKey } from './editor-config/comments'
 // import applyDevTools from 'prosemirror-dev-tools'
 
-function useForceUpdate() {
-  const [, setValue] = useState(0)
-  return () => setValue((value) => ++value)
-}
+// function useForceUpdate() {
+//   const [, setValue] = useState(0)
+//   return () => setValue((value) => ++value)
+// }
 
-export default function Editor(props) {
-  const {
-    post,
-    options,
-    onChange,
-    isEditable,
-    render,
-    field,
-    autoFocus,
-  } = props
+// export default function Editor(props) {
+//   const {
+//     post,
+//     options,
+//     onChange,
+//     isEditable,
+//     render,
+//     field,
+//     autoFocus,
+//   } = props
 
-  console.log(field.toUpperCase() + ' EDITOR PROPS', props)
+//   console.log(field.toUpperCase() + ' EDITOR PROPS', props)
 
-  const forceUpdate = useForceUpdate()
+//   const forceUpdate = useForceUpdate()
 
-  const editorRef = useRef()
+//   const editorRef = useRef()
 
-  const getView = () => view
+//   const getView = () => view
 
-  const view = new EditorView(null, {
-    state: EditorState.create({
-      schema: options.schema,
-      doc: options.doc,
-      plugins: options.setupPlugins(getView),
-      field: field,
-    }),
-    dispatchTransaction: (transaction) => {
-      const oldComments = commentPluginKey.getState(view.state)
-      const { state, transactions } = view.state.applyTransaction(transaction)
-      view.updateState(state)
-      const newComments = commentPluginKey.getState(state)
-      if (
-        transactions.some((tr) => tr.docChanged) ||
-        newComments !== oldComments
-      ) {
-        onChange(state.doc, state, field)
-      }
-      forceUpdate()
-    },
-    editable: function (state) {
-      return isEditable
-    },
-    handleDOMEvents: function (view, event) {
-      return true
-    },
-  })
+//   const view = new EditorView(null, {
+//     state: EditorState.create({
+//       schema: options.schema,
+//       doc: options.doc,
+//       plugins: options.setupPlugins(getView),
+//       field: field,
+//     }),
+//     dispatchTransaction: (transaction) => {
+//       const oldComments = commentPluginKey.getState(view.state)
+//       const { state, transactions } = view.state.applyTransaction(transaction)
+//       view.updateState(state)
+//       const newComments = commentPluginKey.getState(state)
+//       if (
+//         transactions.some((tr) => tr.docChanged) ||
+//         newComments !== oldComments
+//       ) {
+//         onChange(state.doc, state, field)
+//       }
+//       forceUpdate()
+//     },
+//     editable: function (state) {
+//       return isEditable
+//     },
+//     handleDOMEvents: function (view, event) {
+//       return true
+//     },
+//   })
 
-  // applyDevTools(view)
+//   // applyDevTools(view)
 
-  useEffect(() => {
-    editorRef.current.appendChild(view.dom)
-    if (autoFocus) view.focus()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+//   useEffect(() => {
+//     editorRef.current.appendChild(view.dom)
+//     if (autoFocus) view.focus()
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [])
 
-  useEffect(() => {
-    view.state.doc = options.doc
-    const newState = EditorState.create({
-      schema: options.schema,
-      doc: options.doc,
-      plugins: view.state.plugins,
-      field: field,
-      selection: view.state.selection,
+//   useEffect(() => {
+//     console.log('i am here')
+//     view.state.doc = options.doc
+//     const newState = EditorState.create({
+//       schema: options.schema,
+//       doc: options.doc,
+//       plugins: view.state.plugins,
+//       field: field,
+//       selection: view.state.selection,
+//     })
+//     view.updateState(newState)
+//     view.focus()
+
+//     return () => {
+//       view.destroy()
+//     }
+//   }, [post]// <--idk what this is suppoed to be)
+
+//   const editor = <div ref={editorRef} />
+
+//   return render ? render({ editor, view }) : editor
+// }
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props)
+    const { field } = props
+    console.log(field.toUpperCase() + ' EDITOR PROPS', props)
+
+    this.editorRef = React.createRef()
+
+    const getView = () => this.view
+
+    this.view = new EditorView(null, {
+      // prosemirror options = { plugins, schema, comments: { comments: [] } }
+      state: EditorState.create({
+        ...props.options,
+        field: props.field,
+        plugins: props.options.setupPlugins(getView),
+      }),
+      dispatchTransaction: (transaction) => {
+        const oldComments = commentPluginKey.getState(this.view.state)
+
+        const { state, transactions } = this.view.state.applyTransaction(
+          transaction
+        )
+
+        this.view.updateState(state)
+
+        const newComments = commentPluginKey.getState(state)
+
+        if (
+          transactions.some((tr) => tr.docChanged) ||
+          newComments !== oldComments
+        ) {
+          this.props.onChange(state.doc, state, props.field)
+        }
+
+        this.forceUpdate()
+        // this.view.focus()
+      },
+      editable: function (state) {
+        return this.props.isEditable
+      }.bind(this),
+      handleDOMEvents: function (view, event) {
+        console.log('eve', event)
+        return true
+      },
     })
-    view.updateState(newState)
-    view.focus()
 
-    return () => {
-      view.destroy()
+    // applyDevTools(this.view)
+  }
+
+  componentDidMount() {
+    this.editorRef.current.appendChild(this.view.dom)
+    if (this.props.autoFocus) this.view.focus()
+  }
+
+  componentWillUnmount() {
+    this.view.destroy()
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (snapshot) {
+      this.view.state.doc = this.props.options.doc
+      const newState = EditorState.create({
+        ...this.props.options,
+        field: this.props.field,
+        plugins: this.view.state.plugins,
+        selection: this.view.state.selection,
+      })
+      this.view.updateState(newState)
+      this.view.focus()
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [post])
+  }
 
-  const editor = <div ref={editorRef} />
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    if (this.props.post !== prevProps.post) {
+      return this.props
+    }
+    return null
+  }
 
-  return render ? render({ editor, view }) : editor
+  render() {
+    const editor = <div ref={this.editorRef} />
+
+    return this.props.render
+      ? this.props.render({
+          editor,
+          view: this.view,
+        })
+      : editor
+  }
 }
+
+export default Editor
--- app/javascript/components/Floater.js
+++ app/javascript/components/Floater.js
@@ -1,136 +1,53 @@
 import React, { useRef, useEffect, useState } from 'react'
 
-// export default function Floater({ view }) {
-//   const [style, setStyle] = useState({ left: 0, right: 0 })
+export default function Floater(props) {
+  const { view, children } = props
+  const [style, setStyle] = useState({ left: 0, top: 0 })
 
-//   const menuRef = useRef()
+  const menuRef = useRef()
 
-//   useEffect(() => {
-//     const calculateStyle = () => {
-//       const selection = view.state
+  useEffect(() => {
+    const calculateStyle = (view) => {
+      const { selection } = view.state
 
-//       let mathNodeIsSelected =
-//         selection.node &&
-//         selection.node.type &&
-//         selection.node.type.name &&
-//         (selection.node.type.name === 'math_block' ||
-//           selection.node.type.name === 'math_inline')
+      let mathNodeIsSelected =
+        selection.node &&
+        selection.node.type &&
+        selection.node.type.name &&
+        (selection.node.type.name === 'math_block' ||
+          selection.node.type.name === 'math_inline')
 
-//       if (!selection || selection.empty || mathNodeIsSelected) {
-//         return {
-//           left: -1000,
-//           top: 0,
-//         }
-//       }
-
-//       const { offsetWidth, offsetHeight } = menuRef.current
-//       const anchor = view.coordsAtPos(selection.$anchor.pos)
-//       const scrollY = window.scrollY
-
-//       let top =
-//         anchor.top - 10 > 0
-//           ? anchor.top - 110 + scrollY - offsetHeight
-//           : anchor.top + 20
-
-//       let left =
-//         window.innerWidth - offsetWidth < anchor.left
-//           ? anchor.left - offsetWidth - 120
-//           : anchor.left
-
-//       if (left < 5) {
-//         left = 5
-//       }
-
-//       return {
-//         left: left,
-//         top: top,
-//       }
-//     }
-//     setStyle(calculateStyle())
-//   }, [view])
-
-//   return (
-//     <div ref={menuRef} className="floater" style={style}>
-//       {view}
-//     </div>
-//   )
-// }
-
-class Floater extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      style: {
-        left: 0,
-        top: 0,
-      },
-    }
-
-    this.menuRef = React.createRef()
-  }
-
-  componentDidMount() {
-    this.setState({
-      style: this.calculateStyle(this.props),
-    })
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    this.setState({
-      style: this.calculateStyle(nextProps),
-    })
-  }
-
-  render() {
-    return (
-      <div ref={this.menuRef} className="floater" style={this.state.style}>
-        {this.props.children}
-      </div>
-    )
-  }
-
-  calculateStyle(props) {
-    const { view } = props
-    const { selection } = view.state
-
-    let mathNodeIsSelected =
-      selection.node &&
-      selection.node.type &&
-      selection.node.type.name &&
-      (selection.node.type.name === 'math_block' ||
-        selection.node.type.name === 'math_inline')
-
-    if (!selection || selection.empty || mathNodeIsSelected) {
-      return {
-        left: -1000,
-        top: 0,
+      if (!selection || selection.empty || mathNodeIsSelected) {
+        return {
+          left: -1000,
+          top: 0,
+        }
       }
+      const { offsetWidth, offsetHeight } = menuRef.current
+      const anchor = view.coordsAtPos(selection.$anchor.pos)
+      const scrollY = window.scrollY
+
+      let top =
+        anchor.top - 10 > 0
+          ? anchor.top - 110 + scrollY - offsetHeight
+          : anchor.top + 20
+
+      let left =
+        window.innerWidth - offsetWidth < anchor.left
+          ? anchor.left - offsetWidth - 120
+          : anchor.left
+
+      if (left < 5) {
+        left = 5
+      }
+      return { left: left, top: top }
     }
+    setStyle(calculateStyle(view))
+  }, [props, view])
 
-    const { offsetWidth, offsetHeight } = this.menuRef.current
-    const anchor = view.coordsAtPos(selection.$anchor.pos)
-    const scrollY = window.scrollY
-
-    let top =
-      anchor.top - 10 > 0
-        ? anchor.top - 110 + scrollY - offsetHeight
-        : anchor.top + 20
-
-    let left =
-      window.innerWidth - offsetWidth < anchor.left
-        ? anchor.left - offsetWidth - 120
-        : anchor.left
-
-    if (left < 5) {
-      left = 5
-    }
-
-    return {
-      left: left,
-      top: top,
-    }
-  }
+  return (
+    <div ref={menuRef} className="floater" style={style}>
+      {children}
+    </div>
+  )
 }
-
-export default Floater
