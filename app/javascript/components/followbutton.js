6728f5fd475ac04976ba9701a7c8bb54a3935571
cindy <ciindy.wu@gmail.com>
2020-10-12 15:32:00 -1000

    Finish followbutton component refactor

--- app/javascript/components/followbutton.js
+++ app/javascript/components/followbutton.js
@@ -3,229 +3,111 @@ import saRequest from '../utils/saRequest'
 import Modal from 'react-modal'
 
 // TODO: add debounce and rate limiter
-//{ objectId, objectType, following, buttonClass, currentUser }
-// export default function FollowButton(props) {
-//   const { objectId, objectType, currentUser } = props
+export default function FollowButton(props) {
+  const { objectId, objectType, currentUser } = props
 
-//   const [following, setFollowing] = useState(props.following || false)
-//   const [showModal, setShowModal] = useState(false)
-//   const [error, setError] = useState(null)
+  const [following, setFollowing] = useState(props.following || false)
+  const [showModal, setShowModal] = useState(false)
+  const [error, setError] = useState(null)
 
-//   Modal.setAppElement('#root')
+  Modal.setAppElement('#root')
 
-//   useEffect(() => {
-//     let el = document.querySelector('.hide-follow-button')
-//     el.parentNode.removeChild(el)
-//   })
-
-//   const followData = () => {
-//     return {
-//       user_id: currentUser,
-//       follow: {
-//         following_type: objectType,
-//         following_id: objectId,
-//       },
-//     }
-//   }
-
-//   const setFollow = () => {
-//     if (currentUser) {
-//       let data = followData()
-//       let url = '/users/' + objectId + '/follow'
-
-//       saRequest
-//         .post(url)
-//         .send(data)
-//         .set('accept', 'application/json')
-//         .then((res) => {
-//           setFollowing(true)
-//         })
-//         .catch((err) => {
-//           console.log('err.message', err.message)
-//           setError(err.message)
-//         })
-//     }
-//   }
-
-//   const setUnfollow = () => {
-//     if (currentUser) {
-//       let data = followData()
-//       let url = '/users/' + objectId + '/unfollow'
-
-//       saRequest
-//         .post(url)
-//         .send(data)
-//         .set('accept', 'application/json')
-//         .then((res) => {
-//           setFollowing(false)
-//         })
-//         .catch((err) => {
-//           console.log('err.message', err.message)
-//           setError(err.message)
-//         })
-//     }
-//   }
-
-//   const handleClick = (e) => {
-//     if (!currentUser) {
-//       setShowModal(true)
-//     } else {
-//       e.preventDefault()
-//       e.stopPropagation()
-//       return following ? setUnfollow() : setFollow()
-//     }
-//   }
-
-//   let classNames = 'btn btn-primary'
-//   if (following) {
-//     classNames += ' following'
-//   }
-
-//   return (
-//     <div>
-//       <Modal
-//         isOpen={showModal}
-//         className="clear-modal-container"
-//         // onRequestClose={() => this.setState({ showModal: false })}
-//         onRequestClose={() => setShowModal(false)}
-//         shouldCloseOnOverlayClick={true}
-//       >
-//         <div className="reg-modal-container animated fadeIn">
-//           <div className="login-message ">
-//             <p>You must be logged in to follow this user.</p>
-//           </div>
-//         </div>
-//       </Modal>
-
-//       <button
-//         className={classNames}
-//         onClick={(e) => handleClick(e)}
-//         id="followbutton"
-//       >
-//         {following && <span></span>}
-//         {!following && (
-//           <span>
-//             <i className="fa fa-user-friends mr-2" />
-//           </span>
-//         )}
-//       </button>
-//     </div>
-//   )
-// }
-
-class FollowButton extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      object_id: this.props.objectId,
-      object_type: this.props.objectType,
-      following: this.props.following || false,
-      showModal: false,
-    }
-    Modal.setAppElement('#root')
-  }
-
-  componentDidMount() {
-    var el = document.querySelector('.hide-follow-button')
-    console.log('el', el)
+  useEffect(() => {
+    let el = document.querySelector('.hide-follow-button')
     el.parentNode.removeChild(el)
-  }
+  }, [])
 
-  followData() {
+  const followData = () => {
     return {
-      user_id: this.props.currentUser,
+      user_id: currentUser,
       follow: {
-        following_type: this.state.objectType,
-        following_id: this.state.objectId,
+        following_type: objectType,
+        following_id: objectId,
       },
     }
   }
 
-  setFollow() {
-    if (this.props.currentUser) {
-      var data = this.followData()
-      var url = '/users/' + this.props.objectId + '/follow'
+  const setFollow = () => {
+    if (currentUser) {
+      let data = followData()
+      let url = '/users/' + objectId + '/follow'
 
       saRequest
         .post(url)
         .send(data)
         .set('accept', 'application/json')
         .then((res) => {
-          this.setState({ following: true })
+          setFollowing(true)
         })
         .catch((err) => {
-          console.log(err.message)
-          this.setState({ error: err.message })
+          console.log('err.message', err.message)
+          setError(err.message)
         })
     }
   }
 
-  setUnfollow() {
-    if (this.props.currentUser) {
-      var data = this.followData()
-      var url = '/users/' + this.props.objectId + '/unfollow'
+  const setUnfollow = () => {
+    if (currentUser) {
+      let data = followData()
+      let url = '/users/' + objectId + '/unfollow'
 
       saRequest
         .post(url)
         .send(data)
         .set('accept', 'application/json')
         .then((res) => {
-          this.setState({ following: false })
+          setFollowing(false)
         })
         .catch((err) => {
-          console.log(err.message)
-          this.setState({ error: err.message })
+          console.log('err.message', err.message)
+          setError(err.message)
         })
     }
   }
 
-  handleClick(e) {
-    if (!this.props.currentUser) {
-      this.setState({ showModal: true })
+  const handleClick = (e) => {
+    if (!currentUser) {
+      setShowModal(true)
     } else {
       e.preventDefault()
       e.stopPropagation()
-      return this.state.following ? this.setUnfollow() : this.setFollow()
+      return following ? setUnfollow() : setFollow()
     }
   }
 
-  render() {
-    var classnames = 'btn btn-primary'
-    if (this.state.following) {
-      classnames += ' following'
-    }
+  let classNames = 'btn btn-primary'
+  if (following) {
+    classNames += ' following'
+  }
 
-    return (
-      <div>
-        <Modal
-          isOpen={this.state.showModal}
-          className="clear-modal-container"
-          onRequestClose={() => this.setState({ showModal: false })}
-          shouldCloseOnOverlayClick={true}
-        >
-          <div className="reg-modal-container animated fadeIn">
-            <div className="login-message ">
-              <p>You must be logged in to follow this user.</p>
-            </div>
+  return (
+    <div>
+      <Modal
+        isOpen={showModal}
+        className="clear-modal-container"
+        // onRequestClose={() => this.setState({ showModal: false })}
+        onRequestClose={() => setShowModal(false)}
+        shouldCloseOnOverlayClick={true}
+      >
+        <div className="reg-modal-container animated fadeIn">
+          <div className="login-message ">
+            <p>You must be logged in to follow this user.</p>
           </div>
-        </Modal>
+        </div>
+      </Modal>
 
-        <button
-          className={classnames}
-          onClick={(e) => this.handleClick(e)}
-          id="followbutton"
-        >
-          {this.state.following && <span></span>}
-          {!this.state.following && (
-            <span>
-              <i className="fa fa-user-friends mr-2" />
-            </span>
-          )}
-        </button>
-      </div>
-    )
-  }
+      <button
+        className={classNames}
+        onClick={(e) => handleClick(e)}
+        id="followbutton"
+      >
+        {following && <span></span>}
+        {!following && (
+          <span>
+            <i className="fa fa-user-friends mr-2" />
+          </span>
+        )}
+      </button>
+    </div>
+  )
 }
-
-export default FollowButton
