4c88f7b5e4490555aebbf40e2720a4469a025a80
cindy <ciindy.wu@gmail.com>
2020-10-22 15:36:09 -1000

    Restyles /styleguide and adds DesignSystem react component

---
+++ app/javascript/components/DesignSystem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function DesignSystem() {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-2">Colors</div>
+          <div className="col-md-10">All the colors Jelly uses.</div>
+        </div>
+      </div>
+    </div>
+  )
+}
