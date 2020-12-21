277690ecb9efbfb433156bdcd5790fa765642791
cindy <ciindy.wu@gmail.com>
2020-12-20 21:23:20 -1000

    Adds twitter card for collections

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,15 @@
 module TagsHelper
+  def tag_description(tag)
+    if tag.posts.present?
+      tag.posts.count > 1 ? "A collection of #{tag.posts.count} posts" : "A collection of one post"
+    else
+      "An empty collection"
+    end
+  end
+
+  def tag_text(tag)
+    if tag.text
+      "#{meta_tag_sanitize_and_truncate(@tag.text)}"
+    end
+  end
 end
