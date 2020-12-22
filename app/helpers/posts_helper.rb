cb5a6928cf5c23d3154d0e3cda6eef3312dfba3a
Cindy Wu <ciindy.wu@gmail.com>
2020-12-22 08:34:36 -1000

    Adds twitter card for collections (#375)
    * Adds twitter card for collections
    
    * Updates meta_twitter_description for tags show

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,19 @@
 module PostsHelper
   def meta_description(post)
+    if post.abstract
+      "#{meta_tag_sanitize_and_truncate(post.abstract)}"
+    end
+  end
+  
+  def meta_authors(post)
     if post.authors
       "#{post.authors.truncate(120)}"
+    end
+  end
+
+  def meta_title(post)
+    if post.title.present?
+      "#{meta_tag_sanitize_and_truncate(post.title)}"
     end
   end
 
--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,11 @@
 module TagsHelper
+  def tag_description(username)
+    "A collection created by @#{@username}"
+  end
+
+  def tag_text(tag)
+    if tag.text
+      "#{meta_tag_sanitize_and_truncate(@tag.text)}"
+    end
+  end
 end
