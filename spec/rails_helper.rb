758287141f26bf3076f27e6e9df849b6927920c6
Denny Luan <dennyluan@gmail.com>
2021-01-07 14:36:42 -1000

    Adds button to delete posts on edit post page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,6 +13,17 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 
+
+# Requires supporting ruby files with custom matchers and macros, etc, in
+# spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
+# run as spec files by default. This means that files in spec/support that end
+# in _spec.rb will both be required and run as specs, causing the specs to be
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
